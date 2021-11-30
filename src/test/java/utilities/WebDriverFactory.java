e773f3ad3d48d3d464324b3bedffba57d1808caf
bulentkoyuncu <bulentkoyuncuart@gmail.com>
2021-11-29 18:05:30 -0600

    Task is done!

---
+++ src/test/java/utilities/WebDriverFactory.java
@@ -0,0 +1,36 @@
+package utilities;
+
+//TASK: New method creation
+//Method Name: getDriver
+// Static Method
+//Accepts String arg: Browser type
+//   - This arg will determine what type of browser is opened
+//   - if "chrome" passed --> it will open chrome browser
+//   - if "firefox" passed --> it will open firefox browser
+// RETURN TYPE: "WebDriver"
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverFactory {
+
+    public static WebDriver getDriver(String browserType) {
+
+        if (browserType.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+
+            return new ChromeDriver();
+
+        } else if (browserType.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+
+            return new FirefoxDriver();
+
+        } else {
+            System.out.println("Given browser type does not exist.  Driver = null");
+            return null;
+        }
+    }
+}
